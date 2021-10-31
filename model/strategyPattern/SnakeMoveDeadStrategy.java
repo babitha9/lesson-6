e1b00795b547849f70cfc597bdada3400d4efc88
Arthiupperla <arthiupperla1297@gmail.com>
2021-10-31 14:33:59 +0530

    Initial Commit

--- model/strategyPattern/SnakeMoveDeadStrategy.java
+++ model/strategyPattern/SnakeMoveDeadStrategy.java
@@ -3,8 +3,8 @@ package model.strategyPattern;
 public class SnakeMoveDeadStrategy implements SnakeMoveStrategy {
 
     @Override
-    public void moveAlgorithm() {
+    public void moveAlogirthm() {
 
     }
-    
+ 
 }
